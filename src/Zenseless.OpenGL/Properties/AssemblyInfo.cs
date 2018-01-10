36deda6caf71ad61527bb7f8631384f5199df267
scherzer <scherzer@hs-weingarten.de>
2018-01-10 21:00:18 +0100

    OpenGL package

---
+++ src/Zenseless.OpenGL/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Zenseless.OpenGL")]
+[assembly: AssemblyDescription("OpenGL classes for real-time computer graphics and games.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Daniel Scherzer")]
+[assembly: AssemblyProduct("Zenseless.OpenGL")]
+[assembly: AssemblyCopyright("Copyright © 2015 Daniel Scherzer")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("C660F6AF-C100-4169-A9FB-61E1EBE87F34")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyFileVersion("0.1")]
+[assembly: NeutralResourcesLanguage("en")]
+
