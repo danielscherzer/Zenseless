9b19f16dc8c5f27d5fccbc7e76feddabc2f55ab3
scherzer <scherzer@hs-weingarten.de>
2018-01-11 13:15:27 +0100

    added package ContentPipeline

---
+++ src/Zenseless.ContentPipeline/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Zenseless.ContentPipeline")]
+[assembly: AssemblyDescription("A Framework for real-time computer graphics using OpenGL. This framework is created for a university course in computer graphics.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Daniel Scherzer")]
+[assembly: AssemblyProduct("Zenseless.ContentPipeline")]
+[assembly: AssemblyCopyright("Copyright © 2015 Daniel Scherzer")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6810DE78-7764-4C8C-ADA7-E7D31AE6C259")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyFileVersion("0.1")]
+[assembly: NeutralResourcesLanguage("en")]
+
