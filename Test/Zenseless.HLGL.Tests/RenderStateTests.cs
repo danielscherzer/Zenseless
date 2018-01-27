7b52132b2df48c19efc4f43fdc6151bb1830ef50
scherzer <scherzer@hs-weingarten.de>
2018-01-27 14:46:31 +0100

    StateManagerTests.cs -> RenderStateTests

---
+++ Test/Zenseless.HLGL.Tests/RenderStateTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Zenseless.HLGL.Tests
+{
+	using static States;
+
+	[TestClass()]
+	public class RenderStateTests
+	{
+		[TestMethod()]
+		public void RegisterTest()
+		{
+			var renderState = new RenderState();
+			renderState.Register(null, BoolState<IDepthState>.Disabled);
+			Assert.IsFalse(renderState.Get<BoolState<IDepthState>>().IsEnabled);
+			renderState.Set(BoolState<IDepthState>.Enabled);
+			Assert.IsTrue(renderState.Get<BoolState<IDepthState>>().IsEnabled);
+			renderState.Set(BoolState<IDepthState>.Disabled);
+			Assert.IsFalse(renderState.Get<BoolState<IDepthState>>().IsEnabled);
+		}
+
+		[TestMethod()]
+		[ExpectedException(typeof(ArgumentException))]
+		public void UnregisteredTest()
+		{
+			var renderState = new RenderState();
+			var depthTest = renderState.Get<BoolState<IDepthState>>().IsEnabled;
+		}
+
+		[TestMethod()]
+		[ExpectedException(typeof(ArgumentException))]
+		public void RegisterWrongTypeTest()
+		{
+			var renderState = new RenderState();
+			renderState.Register(null, BoolState<IDepthState>.Disabled);
+			var depthTest = renderState.Get<BoolState<IPointSpriteState>>().IsEnabled;
+		}
+	}
+}
