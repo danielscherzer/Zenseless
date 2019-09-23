fcbd4eb424c7e948bc6a9450d59ea7ed66d703d4
scherzer <scherzer@hs-weingarten.de>
2019-09-23 21:02:43 +0200

    NotifyPropertyChanged added SetNotify

--- src/Zenseless.Patterns/NotifyPropertyChanged.cs
+++ src/Zenseless.Patterns/NotifyPropertyChanged.cs
@@ -1,5 +1,6 @@
 ﻿namespace Zenseless.Patterns
 {
+	using System;
 	using System.ComponentModel;
 	using System.Runtime.CompilerServices;
 
@@ -21,6 +22,21 @@
 		protected void RaisePropertyChanged([CallerMemberName] string propertyName = null)
 		{
 			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+		/// <summary>
+		/// Sets a property and Raises the <seealso cref="PropertyChanged"/> event. Shoudl be called from inside the property setter
+		/// </summary>
+		/// <typeparam name="TYPE">The type of the property.</typeparam>
+		/// <param name="valueBackend">The backing variable that contains the property value.</param>
+		/// <param name="value">The new value of the property.</param>
+		/// <param name="action">An action that is executed after changing the property, but before invoking the event.</param>
+		/// <param name="memberName">Auto filled name of the property.</param>
+		protected void SetNotify<TYPE>(ref TYPE valueBackend, TYPE value, Action<TYPE> action = null, [CallerMemberName] string memberName = "")
+		{
+			valueBackend = value;
+			action?.Invoke(value);
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(memberName));
 		}
 	}
 }
